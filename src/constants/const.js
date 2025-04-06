7cc8aff5b7d91f020b47e30444e307dbec8e4437
Ktshka <151648756+Ktshka@users.noreply.github.com>
2025-04-06 18:07:54 +0500

    mod4-task2

--- src/constants/const.js
+++ src/constants/const.js
@@ -8,3 +8,10 @@ export const TimeConstants = {
     MINUTE: 1000 * 60,
   }
 };
+
+export const FilterType = {
+  EVERYTHING: 'everything',
+  FUTURE: 'future',
+  PRESENT: 'present',
+  PAST: 'past'
+};
