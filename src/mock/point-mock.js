12fe861abb1b81adcf8ecf73a5359f180a655b7e
Ktshka <151648756+Ktshka@users.noreply.github.com>
2025-06-07 13:23:25 +0500

    updated point-mock file

--- src/mock/point-mock.js
+++ src/mock/point-mock.js
@@ -1,106 +1,124 @@
-import { getRandomNumber } from '../utils/utils';
+import {getRandomNumber} from '../utils/utils';
 
-const points = [
+const mockPoints = [
   {
     'id': 1,
     'base_price': getRandomNumber(10000, 500),
-    'date_from': '2025-02-10T08:00:00.000Z',
-    'date_to': '2025-02-10T18:00:00.000Z',
+    'date_from': '2025-02-10T01:54:14.857Z',
+    'date_to': '2025-02-10T18:13:14.857Z',
     'destination': 1,
     'is_favorite': false,
-    'offers': ['drive1', 'drive3'],
+    'offers': [],
     'type': 'drive'
   },
   {
     'id': 2,
-    'base_price': getRandomNumber(8000, 400),
-    'date_from': '2025-02-15T12:30:00.000Z',
-    'date_to': '2025-02-15T14:45:00.000Z',
+    'base_price': getRandomNumber(10000, 500),
+    'date_from': '2025-02-11T18:18:14.857Z',
+    'date_to': '2025-02-13T03:59:14.857Z',
     'destination': 2,
     'is_favorite': true,
-    'offers': ['flight1', 'flight3'],
-    'type': 'flight'
-  },
-  {
-    'id': 3,
-    'base_price': getRandomNumber(5000, 200),
-    'date_from': '2025-02-20T09:15:00.000Z',
-    'date_to': '2025-02-20T11:30:00.000Z',
-    'destination': 3,
-    'is_favorite': true,
-    'offers': ['taxi2', 'taxi4'],
-    'type': 'taxi'
-  },
-  {
-    'id': 4,
-    'base_price': getRandomNumber(3000, 100),
-    'date_from': '2025-03-01T14:00:00.000Z',
-    'date_to': '2025-03-01T16:20:00.000Z',
-    'destination': 4,
-    'is_favorite': true,
-    'offers': ['bus1', 'bus3'],
-    'type': 'bus'
-  },
-  {
-    'id': 5,
-    'base_price': getRandomNumber(7000, 300),
-    'date_from': '2025-03-05T07:45:00.000Z',
-    'date_to': '2025-03-05T09:30:00.000Z',
-    'destination': 5,
-    'is_favorite': false,
-    'offers': ['train2', 'train3'],
+    'offers': [
+      'train1',
+      'train2'
+    ],
     'type': 'train'
   },
   {
-    'id': 6,
-    'base_price': getRandomNumber(9000, 600),
-    'date_from': '2025-03-10T20:00:00.000Z',
-    'date_to': '2025-03-11T08:00:00.000Z',
-    'destination': 6,
+    'id': 3,
+    'base_price': getRandomNumber(10000, 500),
+    'date_from': '2025-02-13T18:58:14.857Z',
+    'date_to': '2025-02-14T22:39:14.857Z',
+    'destination': 1,
     'is_favorite': false,
-    'offers': ['ship1', 'ship2'],
-    'type': 'ship'
-  },
-  {
-    'id': 7,
-    'base_price': getRandomNumber(4000, 150),
-    'date_from': '2025-03-15T13:00:00.000Z',
-    'date_to': '2025-03-15T15:30:00.000Z',
-    'destination': 7,
-    'is_favorite': false,
-    'offers': ['checkin1', 'checkin3'],
+    'offers': [],
     'type': 'check-in'
   },
   {
-    'id': 8,
-    'base_price': getRandomNumber(2500, 100),
-    'date_from': '2025-03-20T10:00:00.000Z',
-    'date_to': '2025-03-20T12:00:00.000Z',
-    'destination': 8,
+    'id': 4,
+    'base_price': getRandomNumber(10000, 500),
+    'date_from': '2025-02-16T17:14:14.857Z',
+    'date_to': '2025-02-17T12:48:14.857Z',
+    'destination': 3,
     'is_favorite': false,
-    'offers': ['restaurant1', 'restaurant2'],
+    'offers': [
+      'taxi1',
+      'taxi2',
+      'taxi3'
+    ],
+    'type': 'taxi'
+  },
+  {
+    'id': 5,
+    'base_price': getRandomNumber(10000, 500),
+    'date_from': '2025-02-19T11:32:14.857Z',
+    'date_to': '2025-02-19T19:11:14.857Z',
+    'destination': 4,
+    'is_favorite': true,
+    'offers': [],
+    'type': 'bus'
+  },
+  {
+    'id': 6,
+    'base_price': getRandomNumber(10000, 500),
+    'date_from': '2025-02-21T12:42:14.857Z',
+    'date_to': '2025-02-22T02:09:14.857Z',
+    'destination': 5,
+    'is_favorite': false,
+    'offers': [
+      'bus1',
+      'bus2'
+    ],
+    'type': 'bus'
+  },
+  {
+    'id': 7,
+    'base_price': getRandomNumber(10000, 500),
+    'date_from': '2025-02-22T11:06:14.857Z',
+    'date_to': '2025-02-23T04:29:14.857Z',
+    'destination': 6,
+    'is_favorite': true,
+    'offers': [],
     'type': 'restaurant'
   },
   {
+    'id': 8,
+    'base_price': getRandomNumber(10000, 500),
+    'date_from': '2025-02-24T12:19:14.857Z',
+    'date_to': '2025-02-25T23:24:14.857Z',
+    'destination': 5,
+    'is_favorite': true,
+    'offers': [
+      'checkin1'
+    ],
+    'type': 'check-in'
+  },
+  {
     'id': 9,
-    'base_price': getRandomNumber(1500, 50),
-    'date_from': '2025-03-25T09:00:00.000Z',
-    'date_to': '2025-03-25T18:00:00.000Z',
-    'destination': 9,
-    'is_favorite': false,
-    'offers': [],
-    'type': 'sightseeing'
+    'base_price': getRandomNumber(10000, 500),
+    'date_from': '2025-02-27T17:36:14.857Z',
+    'date_to': '2025-03-01T03:31:14.857Z',
+    'destination': 7,
+    'is_favorite': true,
+    'offers': [
+      'train1',
+      'train2'
+    ],
+    'type': 'train'
   },
   {
     'id': 10,
-    'base_price': getRandomNumber(6000, 200),
-    'date_from': '2025-04-01T11:30:00.000Z',
-    'date_to': '2025-04-01T13:45:00.000Z',
-    'destination': 10,
+    'base_price': getRandomNumber(10001, 500),
+    'date_from': '2025-04-04T04:45:14.857Z',
+    'date_to': '2025-04-04T08:19:14.857Z',
+    'destination': 6,
     'is_favorite': true,
-    'offers': ['drive2', 'drive3'],
-    'type': 'drive'
+    'offers': [
+      'ship1',
+      'ship2'
+    ],
+    'type': 'ship'
   }
 ];
 
-export { points };
+export {mockPoints};
